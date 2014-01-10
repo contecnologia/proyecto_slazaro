bf938fc9d79424a1e9640aeb4a09563d5685f8b9
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2014-01-10 11:44:56 -0600

    Se añade clase ModelAdmin para administrar los posts

--- slazaro/blog/admin.py
+++ slazaro/blog/admin.py
@@ -1,8 +1,54 @@
+# -*- encoding: utf-8 -*-
+
 from django.contrib import admin
 from blog.models import Categoria, Tag, Post, Comentario
+
+class PostAdmin(admin.ModelAdmin):
+
+	fieldsets = [
+		(
+			None,
+			{
+				'fields' : (
+					'titulo', 'contenido',
+				)
+			}
+		),
+		(
+			'Categorización',
+			{
+				'fields' : (
+					'categoria', 'tags', 'estado', 'destacado',
+				)
+			}
+		),
+		(
+			'Extras',
+			{
+				'fields' : (
+					'creado', 'modificado',
+				)
+			}
+		),
+	]
+	list_display = ('titulo', 'usuario', 'categoria', 'estado', 'creado', 'modificado')
+	list_editable = ('estado',)
+	list_filter = ('categoria__nombre', 'estado')
+	search_fields = ('titulo', 'contenido', 'categoria__nombre', 'usuario__first_name', 'usuario__last_name',)
+	filter_horizontal  = ('tags',)
+	readonly_fields = ('creado', 'modificado')
+
+
+	def save_model(self, request, obj, form, change):
+
+		if not change:
+
+			obj.usuario = request.user
+
+		obj.save()
 
 
 admin.site.register(Categoria)
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comentario)
