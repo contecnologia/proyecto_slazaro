05c0b6c18c5b9590e818e2ca633c63e0a5803ae7
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2014-01-10 17:49:27 -0600

    Aplicación Galerías

---
+++ slazaro/galerias/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from galerias.models import Galeria, Imagen
+
+class ImagenesInline(admin.TabularInline):
+
+	model           = Imagen
+	extra           = 1
+	fields          = ('thumb', 'imagen', 'descripcion',)
+	readonly_fields = ('thumb',)
+
+
+class GaleriaAdmin(admin.ModelAdmin):
+
+	inlines = [ImagenesInline]
+
+
+admin.site.register(Galeria, GaleriaAdmin)
